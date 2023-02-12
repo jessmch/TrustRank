f6d70b0f28174adc55f54302d6f8eefafc185cd5
Divam Kumar <divamk.business@gmail.com>
2023-02-11 23:49:26 -0800

    require() statement in smart contract actually works

--- contracts/ReviewClass.sol
+++ contracts/ReviewClass.sol
@@ -13,16 +13,23 @@ contract ReviewClass {
     // Get and set reviews
     mapping(uint => ReviewObject) public reviews;
 
+    // Get the list of order numbers
+    mapping(uint => bool) public hm_orderNumber_bool;
+
     // Counter for the number of reviews
     uint public reviewCount;
 
     constructor () public {
         addReview(1, 848, "Headphone", "Great product", 5);
-        addReview(2, 920, "Watch", "Bad product", 1);
+        addReview(2, 849, "Watch", "Bad product", 1);
+        addReview(1, 850, "Headphone", "Mid product", 3);
     }
 
     function addReview (uint _product_id, uint _order_number, string _product_name, string _review_message, uint _num_stars) private {
+        require(!hm_orderNumber_bool[_order_number]);
+        require(_num_stars >= 1 && _num_stars <= 5);
         reviewCount ++;
         reviews[reviewCount] = ReviewObject(_product_id, _order_number, _product_name, _review_message, _num_stars);
+        hm_orderNumber_bool[_order_number] = true;
     }
 }
--- test/reviews.js
+++ test/reviews.js
@@ -3,11 +3,11 @@ var ReviewClass = artifacts.require("./ReviewClass.sol");
 contract("ReviewClass", function(accounts) {
   var reviewClassInstance;
 
-  it("initializes with two reviews", function() {
+  it("initializes with three reviews", function() {
     return ReviewClass.deployed().then(function(instance) {
       return instance.reviewCount();
     }).then(function(count) {
-      assert.equal(count, 2);
+      assert.equal(count, 3);
     });
   });
 
@@ -24,10 +24,30 @@ contract("ReviewClass", function(accounts) {
       return reviewClassInstance.reviews(2);
     }).then(function(review) {
       assert.equal(review[0], 2, "contains the correct productID");
-      assert.equal(review[1], 920, "contains the correct orderNumber");
+      assert.equal(review[1], 849, "contains the correct orderNumber");
       assert.equal(review[2], "Watch", "contains the correct productName");
       assert.equal(review[3], "Bad product", "contains the correct reviewMessage");
       assert.equal(review[4], 1, "contains the correct numStars");
+      return reviewClassInstance.reviews(3);
+    }).then(function(review) {
+      assert.equal(review[0], 1, "contains the correct productID");
+      assert.equal(review[1], 848, "contains the correct orderNumber");
+      assert.equal(review[2], "Headphone", "contains the correct productName");
+      assert.equal(review[3], "Mid product", "contains the correct reviewMessage");
+      assert.equal(review[4], 3, "contains the correct numStars");
     });
   });
+
+  it("it initializes the hashmap of reviewed hm_orderNumber_bool with the correct values", function() {
+    return ReviewClass.deployed().then(function(instance) {
+      reviewClassInstance = instance;
+      return reviewClassInstance.hm_orderNumber_bool(848);
+    }).then(function(review) {
+      assert.equal(review[0], true, "contains the correct bool");
+      return reviewClassInstance.hm_orderNumber_bool(849);
+    }).then(function(review) {
+      assert.equal(review[0], true, "contains the correct bool");
+    });
+  });
+
 });
